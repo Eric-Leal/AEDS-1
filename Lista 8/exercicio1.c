38e260459a99689b70607d8294c45e6c5598fa78
Eric-Leal <ericgleal@gmail.com>
2024-10-21 08:45:55 -0300

    Lista 8

---
+++ Lista 8/exercicio1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    char nome[70];
+    char nomeInvertido[70];
+
+    printf("Escreva um nome:");
+    fgets(nome, 70, stdin);
+    int n = strlen(nome);
+
+    for(int i=n-1,j=0; i>=0; i--,j++){
+        nomeInvertido[j] = nome[i];
+        printf("%c", nomeInvertido[j]);
+    }
+
+   // }
+
+    return 0;
+
+}
+
+
+
+
