b6de281ad501124c27c005b682f66dcb0efe88a7
Eric-Leal <ericgleal@gmail.com>
2024-10-23 12:29:40 -0300

    Update exercicio4.c

--- Lista 8/exercicio4.c
+++ Lista 8/exercicio4.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+    char str[100];
+
+    printf("Digite uma cadeia de caracteres: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0;  i< strlen(str); i++) {
+        str[i] = toupper(str[i]);
+    }
+
+    printf("Cadeia em maiusculo: %s", str);
+
+    return 0;
+}
 
