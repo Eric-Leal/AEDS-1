60cde27c253d93d40b8406c31cb8dd483ab99e6e
Eric-Leal <ericgleal@gmail.com>
2024-10-23 10:22:00 -0300

    Add files via upload

--- Lista 8/exercicio2.c
+++ Lista 8/exercicio2.c
@@ -4,7 +4,20 @@
 
 int main(){
 
-  int key;
+    char str[100];
+    int key ;
+
+    printf("Escreva uma palavra: ");
+    gets(str);
+
+    printf("Digite uma chave:");
+    scanf("%d", &key);
+
+    for(int i = 0; i<strlen(str); i++){
+        str[i] = (str[i] + key) % 255;
+    }
+
+    printf("String criptografada: %s\n", str);
 
 
 
--- Lista 8/exercicio3.c
+++ Lista 8/exercicio3.c
@@ -4,6 +4,21 @@
 
 int main(){
 
+    int key;
+    char str[100];
+
+    printf("Escreva a string criptografada: ");
+    gets(str);
+
+    printf("Digite a chave:");
+    scanf("%d", &key);
+
+    for(int i = 0; i<strlen(str); i++){
+        str[i] = (str[i] - key) % 255;
+    }
+
+    printf("Palavra inicial: %s\n", str);
+
     return 0;
 
 }
